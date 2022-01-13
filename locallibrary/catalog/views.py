fb5ecc0a0332fe147c053eab415078fed25dc0ef
Mikhaylo09 <mikhaylo09@gmail.com>
2022-01-13 16:24:10 +0300

    Create All books page

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -20,3 +20,25 @@ def index(request):
         'index.html',
         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
     )
+    
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    
+    # context_object_name = 'my_book_list'   # ваше собственное имя переменной контекста в шаблоне
+    # queryset = Book.objects.filter(title__icontains='war')[:5] # Получение 5 книг, содержащих слово 'war' в заголовке
+    # template_name = 'books/my_arbitrary_template_name_list.html'  # Определение имени вашего шаблона и его расположения
+    
+    # def get_queryset(self):
+        # return Book.objects.filter(title__icontains='war')[:5] # Получить 5 книг, содержащих 'war' в заголовке
+        
+    # def get_context_data(self, **kwargs):
+        # В первую очередь получаем базовую реализацию контекста
+        # context = super(BookListView, self).get_context_data(**kwargs)
+        # Добавляем новую переменную к контексту и инициализируем её некоторым значением
+        # context['some_data'] = 'This is just some data'
+        # return context
+        
+class BookDetailView(generic.DetailView):
+    model = Book
