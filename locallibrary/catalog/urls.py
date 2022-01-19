b4252f918d5d15c14babb394176f45b560ff42a9
Mikhaylo09 <mikhaylo09@gmail.com>
2022-01-19 21:34:33 +0300

    Add create user

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -30,3 +30,7 @@ urlpatterns += [
     path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book-update'),
     path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book-delete'),
 ]
+
+urlpatterns += [
+    path('register/', views.register, name='register'),
+]
